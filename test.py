98761fde1e73fd7000c0733a64c61af63dac9d7f
raizento <alexander.molitor@edu.bib.de>
2020-01-29 12:56:12 +0100

    a

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from riotwatcher import RiotWatcher, ApiError
 import json
 
-watcher = RiotWatcher('RGAPI-ef5a14f2-1eb9-47f8-ac16-fa84e63492d5')
+watcher = RiotWatcher('')
 
 my_region = 'euw1'
 
 me = watcher.summoner.by_name(my_region, 'Cookienator19')
 
+id = me['puuid']
 
-
-print(me['name'])
+my_ranked_stats = watcher.league.positions_by_summoner(my_region, me['id'])
